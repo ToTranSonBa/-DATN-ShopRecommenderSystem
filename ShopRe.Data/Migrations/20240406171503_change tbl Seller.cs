4b196a82e3afa7fa2cf9bdf9fb5377e073231107
nthieu2212 <nthieu2212@gmail.com>
2024-04-07 00:25:38 +0700

    change table seller database

---
+++ ShopRe.Data/Migrations/20240406171503_change tbl Seller.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopRe.Data.Migrations
+{
+    public partial class changetblSeller : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFollowed",
+                table: "Seller");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StoreLevel",
+                table: "Seller",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Seller",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Seller");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StoreLevel",
+                table: "Seller",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFollowed",
+                table: "Seller",
+                type: "bit",
+                nullable: true);
+        }
+    }
+}
