e4bcaad729159a9cabfdf8dca79d9acfddca08dd
nthieu2212 <nthieu2212@gmail.com>
2024-04-07 20:36:19 +0700

    update tbl product - add "shortUrl" atb

---
+++ ShopRe.Data/Migrations/20240407133339_change tbl Product.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopRe.Data.Migrations
+{
+    public partial class changetblProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShortUrl",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShortUrl",
+                table: "Product");
+        }
+    }
+}
