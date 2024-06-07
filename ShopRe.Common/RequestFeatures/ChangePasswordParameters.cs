1228bbd21b35dd6b49d0583f1b8e54b4b0415c4f
QuocMaChe <quocmache@gmail.com>
2024-06-07 09:04:39 +0700

    fix API changePassword

--- ShopRe.Common/RequestFeatures/ChangePasswordParameters.cs
+++ ShopRe.Common/RequestFeatures/ChangePasswordParameters.cs
@@ -9,7 +9,7 @@ namespace ShopRe.Common.RequestFeatures
     public class ChangePasswordParameters
     {
         public string PasswordOld {  get; set; }
-        public string PasswordNewConfirm { get; set; }
         public string PasswordNew { get; set; }
+        public string PasswordNewConfirm { get; set; }
     }
 }
