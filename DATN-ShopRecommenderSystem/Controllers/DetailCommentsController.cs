d967bd91b2a0a60e96443872f85545d10a64fc6e
QuocMaChe <quocmache@gmail.com>
2024-06-12 13:58:16 +0700

    fix Service Folder

--- DATN-ShopRecommenderSystem/Controllers/DetailCommentsController.cs
+++ DATN-ShopRecommenderSystem/Controllers/DetailCommentsController.cs
@@ -1,5 +1,4 @@
-﻿using DATN_ShopRecommenderSystem.Service;
-using Microsoft.AspNetCore.Cors;
+﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
