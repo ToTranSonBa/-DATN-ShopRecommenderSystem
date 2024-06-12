b009107d14aedc19ded0a98f22fcc628c108ec02
QuocMaChe <quocmache@gmail.com>
2024-06-12 19:03:39 +0700

    update getListOrderOfUser

--- ShopRe.Common/DTOs/OrderItemsDTO.cs
+++ ShopRe.Common/DTOs/OrderItemsDTO.cs
@@ -12,6 +12,7 @@ namespace ShopRe.Common.DTOs
         public int Id { get; set; }
         public int? Quantity { get; set; }
         public decimal? Price { get; set; }
+        public string? Image {  get; set; }
         public ProductDTO Product { get; set; } 
         public ProductOptionValuesDTO OptionValues { get; set; }
     }
